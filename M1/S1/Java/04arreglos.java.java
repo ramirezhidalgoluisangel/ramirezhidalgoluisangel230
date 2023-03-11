4c3fd72002e8d56de1327ef6d782b19b03d2135a
ramirezhidalgoluisangel <126732767+ramirezhidalgoluisangel@users.noreply.github.com>
2023-03-10 18:54:41 -0600

    Add files via upload

---
+++ M1/S1/Java/04arreglos.java.java
@@ -0,0 +1,15 @@
+ // PRÁCTICA #4     I
+import java.io.*;
+class Vector 
+{
+public static void main(String[ ] args )
+{
+int[ ] números = new int[3] ;
+números [0] =5;
+números [1] =9;
+números [2] =2;
+System.out.println("El primer elemento del arreglo es: "+números[0]);
+System.out.println("El primer elemento del arreglo es: "+números[1]);
+System.out.println("El primer elemento del arreglo es: "+números[2]);
+}
+}
---
+++ M1/S1/Java/05arreglos.java.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class Vector
+  {
+   public static void main(String[] args)
+     {
+       int[] numeros = {5, 9, 2};
+       System.out.println("El primer elemento del arreglo es: "+numeros[0]);
+       System.out.println("El primer elemento del arreglo es: "+numeros[1]);
+       System.out.println("El primer elemento del arreglo es: "+numeros[2]);
+     }
+  }
